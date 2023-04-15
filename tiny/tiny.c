7441fcfb0d0873afdb6b7ad199931bc8e75d2603
Ubuntu <ubuntu@ip-172-31-34-30.ap-northeast-2.compute.internal>
2023-04-15 12:02:54 +0000

    home work 11.9

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -111,7 +111,14 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
     strcpy(filename, ".");
     strcat(filename, uri);
     if (uri[strlen(uri) - 1] == '/')
+    {
       strcat(filename, "home.html");
+    }
+    else if (strcmp(uri, "/video") == 0)
+    {
+      strcpy(filename, "home_with_video.html");
+      printf("filename = %s", filename);
+    }
     return 1;
   }
   else /* Dynamic comtent */
@@ -129,6 +136,30 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
     return 0;
   }
 }
+// void serve_static(int fd, char *filename, int filesize)
+// {
+//   int srcfd;
+//   char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+//   /* Send response headers to client */
+//   get_filetype(filename, filetype);
+//   sprintf(buf, "HTTP/1.0 200 OK\r\n");
+//   sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+//   sprintf(buf, "%sConnection: close\r\n", buf);
+//   sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+//   sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+//   Rio_writen(fd, buf, strlen(buf));
+//   printf("Response headers:\n");
+//   printf("%s", buf);
+
+//   /* Send response body to client */
+//   srcfd = Open(filename, O_RDONLY, 0);
+//   srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+//   Close(srcfd);
+//   Rio_writen(fd, srcp, filesize);
+//   Munmap(srcp, filesize);
+// }
+/* homework 11.9 */
 void serve_static(int fd, char *filename, int filesize)
 {
   int srcfd;
@@ -146,11 +177,18 @@ void serve_static(int fd, char *filename, int filesize)
   printf("%s", buf);
 
   /* Send response body to client */
+  // srcfd = Open(filename, O_RDONLY, 0);
+  // srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  // Close(srcfd);
+  // Munmap(srcp, filesize);
+
   srcfd = Open(filename, O_RDONLY, 0);
-  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  srcp = (char *)malloc(filesize);
+  Rio_readn(srcfd, srcp, filesize);
   Close(srcfd);
   Rio_writen(fd, srcp, filesize);
-  Munmap(srcp, filesize);
+  free(srcp);
+  // Close(srcfd);
 }
 /*
  * get_filetype - Derive file type from filename
@@ -165,6 +203,8 @@ void get_filetype(char *filename, char *filetype)
     strcpy(filetype, "image/png");
   else if (strstr(filename, ".jpg"))
     strcpy(filetype, "image/jpeg");
+  else if (strstr(filename, ".mp4"))
+    strcpy(filetype, "image/mp4");
   else
     strcpy(filetype, "text/plain");
 }
@@ -177,7 +217,7 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
   Rio_writen(fd, buf, strlen(buf));
   sprintf(buf, "Server: Tiny Web Server\r\n");
   Rio_writen(fd, buf, strlen(buf));
-  
+
   if (Fork() == 0)
   { /* Child */
     /* Real server would set all CGI vars here */
